9f58d62b282e4d692c9cb90cc516e7ae3b37a742
Pranay-Tej <s.pranayteja@gmail.com>
2024-02-24 18:44:08 +0530

    add github link

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import "./App.css";
 import MemoryGame from "./MemoryGame";
+import githubIcon from "./assets/github-mark.png";
 
 function App() {
-  return <MemoryGame />;
+  return (
+    <>
+      <MemoryGame />
+      <a
+        target="_blank"
+        href="https://github.com/Pranay-Tej/react-games"
+        title="GitHub"
+        style={{ maxWidth: "40px", margin: "1rem auto", display: "block" }}
+      >
+        <img src={githubIcon} alt="github" style={{ maxWidth: "100%" }} />
+      </a>
+    </>
+  );
 }
 
 export default App;
